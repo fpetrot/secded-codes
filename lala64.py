325bb6ac9d49dfcb57bfd695a23868e64f3f6e91
Frédéric Pétrot <frederic.petrot@univ-grenoble-alpes.fr>
2025-04-05 13:02:13 +0200

    Now generating correct verilog files looking like lowrisc ones

--- lala64.py
+++ lala64.py
@@ -16,103 +16,113 @@ k = 64   # Number of bits to protect
 r = 8    # Number of check bits according to theory
 
 # Dump something looking like Verilog
-def dump_verilog(pcm):
-    with open('compute_checkbits_lala.v', 'w') as f:
+# Following the lowrisc example, we concatenate the check bits
+# on the MSB, as opposed to what the matrix says, but this
+# should not change much
+def dump_verilog(pcm, mpc):
+    with open('prim_secded_lala_73_64_enc.v', 'w') as f:
         with redirect_stdout(f):
-            print("module compute_checkbits_lala (\n",
-                  "    input  wire [63:0] d,\n",
-                  "    output wire  [8:0] c\n);\n")
-
+            print("module prim_secded_lala_73_64_enc (\n"
+                  "    input  logic [63:0] in,\n"
+                  "    output logic [72:0] out\n);\n"
+                  "    always_comb begin : p_encode\n"
+                  "        out[63:0] = in;" )
             for c in range(0, r + 1):
-                print(f"assign c[{c}] = ", end='')
-                start = 0;
-                for d in range(0, k):
-                    if pcm[c,d] == 1:
-                        if start != 0:
-                            print(" ^ ", end='')
-                        start = 1;
-                        print(f"d[{k - 1 - d}]", end='')
-                print(";")
-            print("endmodule")
+                print(f"        out[{72 - c}] = ^(in & 64'h{l2u(pcm[c][9:73]):016x});")
+            print("    end\nendmodule : prim_secded_lala_73_64_enc")
 
-    with open('compute_syndrome_lala.v', 'w') as f:
+    with open('prim_secded_lala_73_64_dec.v', 'w') as f:
         with redirect_stdout(f):
-            print("module compute_syndrome_lala (\n",
-                  "    input  wire [63:0] d,\n",
-                  "    input  wire  [8:0] c,\n",
-                  "    output wire  [8:0] s\n);\n")
+            print("module prim_secded_lala_73_64_dec (\n"
+                  "    input  logic [72:0] in,\n"
+                  "    output logic [63:0] d_o,\n"
+                  "    output logic  [8:0] syndrome_o,\n"
+                  "    output logic  [1:0] err_o \n);\n")
 
             for s in range(0, r + 1):
-                print(f"assign s[{s}] = ", end='')
-                start = 0;
-                for d in range(0, k + r + 1):
-                    if pcm[s,d] == 1:
-                        if start != 0:
-                            print(" ^ ", end='')
-                        start = 1;
-                        if d < k:
-                            print(f"d[{k - 1 - d}]", end='')
-                        else:
-                            print(f"c[{s}]", end='')
-                print(";")
-            print("endmodule")
+                print(f"    assign syndrome_o[{8 - s}] = ^(in & 73'h{l2u(pcm[s][0:73]):019x});")
+            for d in range(0, k):
+                print(f"    assign d_o[{d}] = (syndrome_o == 9'h{l2i(pcm[:,72 - d]):03x}) ^ in[{d}];")
 
-    with open('check_syndrome_lala.v', 'w') as f:
+            print("    logic       ne = syndrome_o == 0;")
+            # We don't really care whether the error is in the checkbits or residue bit
+            # print("logic       m = syndrome_o[8];")
+            # print("/* verilator lint_off WIDTHEXPAND */")
+            # print("logic [4:0] p = syndrome_o[7] + syndrome_o[6] + syndrome_o[5] + syndrome_o[4] + syndrome_o[3] + syndrome_o[2] + syndrome_o[1] + syndrome_o[0];")
+            # print("logic       ce = (m == 0 && p == 1) || (m == 1 && p == 0);")
+            # print("logic       se = (m == 0 && p == 3) || (m == 1 && p == 2);")
+            print("    logic se = ^syndrome_o;")
+            print("    assign err_o = {~(ne | se), se};")
+            print("endmodule : prim_secded_lala_73_64_dec")
+
+    with open('prim_secded_lala_73_64_cor.v', 'w') as f:
         with redirect_stdout(f):
-            print("module check_syndrome_lala (\n",
-                  "    input  wire  [8:0] syndrome,\n",
-                  "    output wire        ne, // no error\n",
-                  "    output wire        ce, // single checkbit error\n",
-                  "    output wire        se, // single bit error\n",
-                  "    output wire        de  // double bit error\n",
-                  ");\n")
+            print("module prim_secded_lala_73_64_cor (\n"
+                  "    input        [72:0] d_i,\n"
+                  "    output logic [72:0] d_o,\n"
+                  "    output logic  [8:0] syndrome_o,\n"
+                  "    output logic  [1:0] err_o);\n")
 
-            print(f"wire [7:0] s = syndrome[8:1];")
-            print(f"wire [4:0] p = s[7] + s[6] + s[5] + s[4] + s[3] + s[2] + s[1] + s[0];")
-            print(f"wire       m = syndrome[0];")
-            print(f"assign ne = syndrome == 0;")
-            print(f"assign ce = (m == 0 && p == 1) || (m == 1 && s == 0);")
-            print(f"assign se = (m == 0 && p == 3) || (m == 1 && p == 2);")
-            print(f"assign de = ~(ne | ce | se);")
-            print("endmodule")
+            for s in range(0, r + 1):
+                print(f"    assign syndrome_o[{8 - s}] = ^(d_i & 73'h{l2u(pcm[s][0:73]):019x});")
+            for d in range(0, k + r + 1):
+                print(f"    assign d_o[{d}] = (syndrome_o == 9'h{l2i(pcm[:,72 - d]):03x}) ^ d_i[{d}];")
 
+            print("    logic       ne = syndrome_o == 0;")
+            print("    logic se = ^syndrome_o;")
+            print("    assign err_o = {~(ne | se), se};")
+            print("endmodule : prim_secded_lala_73_64_cor")
+
+# Make a list of 0s and 1s an integer
+def l2i(l):
+    v = 0
+    t = len(l)
+    for b in l:
+        v = (v << 1) | b
+    return int(v)
+
+def l2ir(l):
+    v = 0
+    t = len(l)
+    for i, b in enumerate(l):
+        v = v | (b << i)
+    return int(v)
+
+# Hack to output a 64-bit unsigned integer
+def l2u(l):
+    t = len(l)
+    v = 1 << t
+    for b in l:
+        v = (v << 1) | b
+    return (v - (1 << t)) & ((1 << t) - 1)
+
+def parity(v):
+    n = v & 1
+    for i in range(1, v.bit_length()):
+        n = n ^ (( v >> i) & 1)
+    return int(n)
 
 # For the check and syndrome functions, order matters
-# because the check bits are appended somewhere,
-# after the lsb in our case
+# because the check bits are appended in front of the data
+# This makes index calculation a bit weird
 
 # Compute check bits
 def compute_checkbits(pcm, v):
     check = np.zeros(shape=r + 1, dtype=np.uint32)
     for i in range(0, r + 1):
-        for j in range(0, k):
+        for j in range(r + 1, r + 1 + k):
             if pcm[i,j] == 1:
-                check[i] = check[i] ^ ((v >> (k - 1 - j)) & 1)
+                check[i] = check[i] ^ ((v >> (k - 1 + r + 1 - j)) & 1)
     return check
 
 # Compute syndrome
 def compute_syndrome(pcm, v):
     check = np.zeros(shape=r + 1, dtype=np.uint32)
     for i in range(0, r + 1):
-        for j in range(0, k + r + 1):
+        for j in range(0, r + 1 + k):
             if pcm[i,j] == 1:
-                check[i] = check[i] ^ ((v >> (k + r + 1 - 1 - j)) & 1)
+                check[i] = check[i] ^ ((v >> (k - 1 + r + 1 - j)) & 1)
     return check
-
-# Make a list of 0s and 1s an integer
-def l2i(l):
-    v = 0
-    t = len(l)
-    # assert t = r + 1, "Argh !"
-    for s, b in enumerate(l):
-        v = v | (b << (t - 1 - s))
-    return int(v)
-
-def parity(v):
-    n = v & 1
-    for i in range(1, k):
-        n = n ^ (( v >> i) & 1)
-    return int(n)
 
 # Using a check without mod3 works when protecting below 84 bits,
 # which is our case.
@@ -148,26 +158,72 @@ def check_error(syndrome):
 # IEE ELECTRONICS LETTERS 23rd June 2005 Vol. 41 No. 13
 # Now only 202 ones, which is 14 less than Hsiao codes
 om = np.zeros(shape=(r + 1, k + r + 1), dtype=np.uint32)
-#        d d d d d d d d  d d d d d d d d  d d d d d d d d  d d d d  d d d d  d d d d d d d d  d d d d d d d d  d d d d d d d d  d d d d d d d d 
-#        6 6 5 5 5 5 5 5  5 5 5 5 5 4 4 4  4 4 4 4 4 4 4 3  3 3 3 3  3 3 3 3  3 3 2 2 2 2 2 2  2 2 2 2 1 1 1 1  1 1 1 1 1 1 0 0  0 0 0 0 0 0 0 0 
-#        3 2 1 9 8 7 6 5  4 3 2 1 0 0 9 8  7 6 5 4 3 2 1 0  9 8 7 6  5 4 3 2  1 0 9 8 7 6 5 4  3 2 1 0 9 8 7 6  5 4 3 2 1 0 9 8  7 6 5 4 3 2 1 0  8 7 6 5 4 3 2 1 0 
-
-om[0] = [1,0,0,0,1,1,0,1, 0,0,0,0,1,0,0,0, 0,0,0,1,0,0,0,0, 0,0,1,0, 0,0,0,1, 0,0,1,0,1,0,1,0, 0,1,1,1,1,0,0,1, 0,1,1,1,0,0,0,0, 1,0,1,0,1,0,0,0, 1,0,0,0,0,0,0,0,0] #23
-om[1] = [0,0,0,0,0,0,0,1, 1,1,0,0,0,0,1,0, 1,0,1,0,1,0,0,0, 0,0,0,0, 0,0,0,1, 1,0,1,0,0,0,1,1, 1,0,1,0,0,0,0,0, 1,1,0,0,1,0,1,0, 0,0,0,0,1,1,1,0, 0,1,0,0,0,0,0,0,0] #23
-om[2] = [0,0,0,1,0,1,0,0, 0,0,0,0,0,1,1,0, 0,0,0,0,0,0,1,1, 0,0,0,1, 0,1,0,0, 0,0,0,1,0,0,0,0, 0,0,0,0,0,0,0,1, 0,0,0,0,1,1,1,1, 0,1,1,1,1,0,1,0, 0,0,1,0,0,0,0,0,0] #20
-om[3] = [1,0,0,0,0,0,0,0, 0,1,1,1,0,0,0,0, 0,0,0,0,0,1,0,1, 1,0,0,0, 0,1,0,0, 0,1,0,1,0,1,0,1, 1,1,0,1,1,1,0,1, 1,1,0,0,0,0,0,0, 0,0,0,0,0,0,0,1, 0,0,0,1,0,0,0,0,0] #22
-om[4] = [0,0,1,1,0,0,1,0, 0,0,0,0,0,0,0,1, 0,0,1,1,0,0,0,0, 1,0,0,0, 1,1,1,1, 1,0,0,0,1,1,0,0, 0,0,0,0,1,1,1,0, 1,0,0,0,0,0,0,1, 1,1,0,0,0,0,0,0, 0,0,0,0,1,0,0,0,0] #22
-om[5] = [0,1,0,0,0,0,1,0, 0,0,0,0,0,0,0,0, 1,1,0,0,0,1,0,0, 0,0,1,1, 0,0,1,0, 0,1,1,0,1,0,0,0, 0,1,0,0,0,1,1,0, 0,0,1,0,0,1,0,0, 0,0,0,0,0,0,1,1, 0,0,0,0,0,1,0,0,0] #19
-om[6] = [0,0,1,0,0,0,0,0, 1,0,0,1,1,1,0,0, 0,1,0,0,0,0,0,0, 0,1,0,0, 1,0,0,0, 0,0,0,1,0,1,1,0, 1,0,0,1,0,0,1,0, 0,0,0,1,1,0,0,0, 1,1,0,1,0,1,0,1, 0,0,0,0,0,0,1,0,0] #22
-om[7] = [0,1,0,0,1,0,0,0, 0,0,1,0,0,0,0,1, 0,0,0,0,1,0,1,0, 0,1,0,0, 1,0,1,0, 1,1,0,0,0,0,0,1, 0,0,1,0,0,0,0,0, 0,0,1,1,0,1,1,1, 0,0,1,1,0,1,0,0, 0,0,0,0,0,0,0,1,0] #22
-om[8] = [1,1,1,1,1,1,1,1, 1,1,1,1,1,1,1,1, 1,1,1,1,1,1,1,1, 1,1,1,1, 0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,1] #29
+#        c c c c c c c c c  d d d d d d d d  d d d d d d d d  d d d d d d d d  d d d d  d d d d  d d d d d d d d  d d d d d d d d  d d d d d d d d  d d d d d d d d
+#                           6 6 5 5 5 5 5 5  5 5 5 5 5 4 4 4  4 4 4 4 4 4 4 3  3 3 3 3  3 3 3 3  3 3 2 2 2 2 2 2  2 2 2 2 1 1 1 1  1 1 1 1 1 1 0 0  0 0 0 0 0 0 0 0 
+#        8 7 6 5 4 3 2 1 0  3 2 1 9 8 7 6 5  4 3 2 1 0 0 9 8  7 6 5 4 3 2 1 0  9 8 7 6  5 4 3 2  1 0 9 8 7 6 5 4  3 2 1 0 9 8 7 6  5 4 3 2 1 0 9 8  7 6 5 4 3 2 1 0   
+                          
+om[0] = [1,0,0,0,0,0,0,0,0, 1,0,0,0,1,1,0,1, 0,0,0,0,1,0,0,0, 0,0,0,1,0,0,0,0, 0,0,1,0, 0,0,0,1, 0,0,1,0,1,0,1,0, 0,1,1,1,1,0,0,1, 0,1,1,1,0,0,0,0, 1,0,1,0,1,0,0,0] #23
+om[1] = [0,1,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,1, 1,1,0,0,0,0,1,0, 1,0,1,0,1,0,0,0, 0,0,0,0, 0,0,0,1, 1,0,1,0,0,0,1,1, 1,0,1,0,0,0,0,0, 1,1,0,0,1,0,1,0, 0,0,0,0,1,1,1,0] #23
+om[2] = [0,0,1,0,0,0,0,0,0, 0,0,0,1,0,1,0,0, 0,0,0,0,0,1,1,0, 0,0,0,0,0,0,1,1, 0,0,0,1, 0,1,0,0, 0,0,0,1,0,0,0,0, 0,0,0,0,0,0,0,1, 0,0,0,0,1,1,1,1, 0,1,1,1,1,0,1,0] #20
+om[3] = [0,0,0,1,0,0,0,0,0, 1,0,0,0,0,0,0,0, 0,1,1,1,0,0,0,0, 0,0,0,0,0,1,0,1, 1,0,0,0, 0,1,0,0, 0,1,0,1,0,1,0,1, 1,1,0,1,1,1,0,1, 1,1,0,0,0,0,0,0, 0,0,0,0,0,0,0,1] #22
+om[4] = [0,0,0,0,1,0,0,0,0, 0,0,1,1,0,0,1,0, 0,0,0,0,0,0,0,1, 0,0,1,1,0,0,0,0, 1,0,0,0, 1,1,1,1, 1,0,0,0,1,1,0,0, 0,0,0,0,1,1,1,0, 1,0,0,0,0,0,0,1, 1,1,0,0,0,0,0,0] #22
+om[5] = [0,0,0,0,0,1,0,0,0, 0,1,0,0,0,0,1,0, 0,0,0,0,0,0,0,0, 1,1,0,0,0,1,0,0, 0,0,1,1, 0,0,1,0, 0,1,1,0,1,0,0,0, 0,1,0,0,0,1,1,0, 0,0,1,0,0,1,0,0, 0,0,0,0,0,0,1,1] #19
+om[6] = [0,0,0,0,0,0,1,0,0, 0,0,1,0,0,0,0,0, 1,0,0,1,1,1,0,0, 0,1,0,0,0,0,0,0, 0,1,0,0, 1,0,0,0, 0,0,0,1,0,1,1,0, 1,0,0,1,0,0,1,0, 0,0,0,1,1,0,0,0, 1,1,0,1,0,1,0,1] #22
+om[7] = [0,0,0,0,0,0,0,1,0, 0,1,0,0,1,0,0,0, 0,0,1,0,0,0,0,1, 0,0,0,0,1,0,1,0, 0,1,0,0, 1,0,1,0, 1,1,0,0,0,0,0,1, 0,0,1,0,0,0,0,0, 0,0,1,1,0,1,1,1, 0,0,1,1,0,1,0,0] #22
+om[8] = [0,0,0,0,0,0,0,0,1, 1,1,1,1,1,1,1,1, 1,1,1,1,1,1,1,1, 1,1,1,1,1,1,1,1, 1,1,1,1, 0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0] #29
 
 # Easier to find which column matches in case of single error
 mo = np.transpose(om)
 
+dump_verilog(om, mo)
 
-dump_verilog(om)
-sys.exit(1)
+def compute_data_enc():
+    for x in [0x0000000000000000, 0xffffffffffffffff, 0xdeadbeefdeadbeef, 0x5555555555555555, 0xaaaaaaaaaaaaaaaa, 0x8badf00dcafebabe]:
+        cb = compute_checkbits(om, x)
+        c = l2i(cb)
+        print(f"{c:03x} {x:016x}")
+    sys.exit(1)
+
+# compute_data_enc()
+
+def compute_data_dec():
+    # No errors
+    for x in [0x0000000000000000, 0xffffffffffffffff, 0xdeadbeefdeadbeef, 0x5555555555555555, 0xaaaaaaaaaaaaaaaa, 0x8badf00dcafebabe]:
+        cb = compute_checkbits(om, x)
+        checkbits = l2i(cb)
+        bv = (checkbits << k) | x
+        sy = compute_syndrome(om, bv)
+        c = l2i(sy)
+        p = parity(c)
+        print(f"sy {c:09b} ({p:01b}) {checkbits:03x} {x:016x}")
+
+    # Single error in data or checkbits
+    for x in [0x0000000000000000, 0xffffffffffffffff, 0xdeadbeefdeadbeef, 0x5555555555555555, 0xaaaaaaaaaaaaaaaa, 0x8badf00dcafebabe]:
+        cb = compute_checkbits(om, x)
+        checkbits = l2i(cb)
+        bv = (checkbits << k) | x
+        for i in range(0, k + r + 1):
+            bv = bv ^ (1 << i)
+            sy = compute_syndrome(om, bv)
+            c = l2i(sy)
+            p = parity(c)
+            print(f"sy {c:09b}-{c:03x} ({p:01b}) {bv:016x}")
+            # Reset bit to proper value for next round
+            bv = bv ^ (1 << i)
+
+    for x in [0xdeadbeefdeadbeef]:
+        cb = compute_checkbits(om, x)
+        checkbits = l2i(cb)
+        bv = (checkbits << k) | x
+        for i in range(0, k + r + 0):
+            for j in range(i + 1, k + r + 1):
+                vb  = bv ^ (1 << i)
+                vb  = vb ^ (1 << j)
+                sy = compute_syndrome(om, vb)
+                c = l2i(sy)
+                p = parity(c)
+                print(f"sy {c:09b} ({p:01b}) {vb:016x}")
+    sys.exit(1)
 
 def dump(pcm):
     for i in range(0, k + r + 1):
@@ -175,7 +231,10 @@ def dump(pcm):
         rl = row.tolist()
         x = l2i(rl)
         print(f"{x:09b}")
-    sys.exit(1)
+
+dump(mo)
+compute_data_dec()
+
 
 # Check that the matrix does not contain twice the same value
 # Crap but since this is a very small array, we'll live with that
@@ -223,8 +282,9 @@ cb = compute_checkbits(om, x)
 checkbits = l2i(cb)
 bv = (x << (r + 1)) | checkbits
 sy = compute_syndrome(om, bv)
-# print("Checkbits ⇒ ", cb)
-# print("Syndrome  ⇒ ", sy)
+print("Syndrome  ⇒ ", sy)
+print("Checkbits ⇒ ", cb)
+sys.exit(1)
 check_error(l2i(sy))
 
 def check_no_error():
